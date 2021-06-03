8b914334fdecf67eb042f5e391b18f8611453c2a
DESKTOP-LUKPNTP\josea <jose.alvarec@mail.udp.cl>
2021-06-02 20:31:56 -0400

    Terminando el formulario web

--- kakebo/forms.py
+++ kakebo/forms.py
@@ -1,14 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField
-from wtforms.fields.core import BooleanField, FloatField, SelectField, StringField
-from wtforms.fields.simple import SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import DateField, StringField, SelectField, SubmitField, FloatField, BooleanField, HiddenField
+from wtforms.validators import DataRequired, Length, ValidationError
+from datetime import date
 
-class MovimientosForm (FlaskForm):
-    fecha = DateField("Fecha", validators=[DataRequired()])
-    concepto = StringField ("Concepto", validators = [DataRequired(), Length(min=10)])
-    Categoria = SelectField ("Categoria", choices= [('SU', 'Supervivencia'), ('OV', 'Ocio/Vicio'),
+def fecha_por_debajo_de_hoy(formulario, campo):
+    hoy = date.today()
+    if campo.data > hoy:
+        raise ValidationError('La fecha {} no puede ser mayor que {}'.format(campo.data, hoy))
+
+class MovimientosForm(FlaskForm):
+    id = HiddenField()
+    fecha = DateField("Fecha", validators = [DataRequired(message="Debe informar una fecha válida"), fecha_por_debajo_de_hoy])
+    concepto = StringField("Concepto", validators = [DataRequired(), Length(min=10)])
+    categoria = SelectField("Categoria", choices=[('00', ''),('SU', 'Supervivencia'), ('OV', 'Ocio/Vicio'), 
                             ('CU', 'Cultura'), ('EX', 'Extras')])
-    cantidad = FloatField ('Cantidad', validators= [DataRequired()])
-    esGasto = BooleanField ('Es Gasto')
-    submit = SubmitField ('Aceptar')
+    cantidad = FloatField("Cantidad", validators = [DataRequired(message="Campo requerido")])
+    esGasto = BooleanField("Es gasto")
+    submit = SubmitField('Aceptar')
+
+class FiltraMovimientosForm(FlaskForm):
+    fechaDesde = DateField("Desde", validators=[fecha_por_debajo_de_hoy], default=date(1, 1, 1))
+    fechaHasta = DateField("Hasta", validators=[fecha_por_debajo_de_hoy], default=date.today())
+    texto = StringField("Concepto")
+    submit = SubmitField("Filtrar")
+
+
+
+    
+    
--- kakebo/views.py
+++ kakebo/views.py
@@ -1,35 +1,121 @@
 #aqui colocamos las rutas para el navegador
 #tenermos que importar del modulo (fichero) kakebo
 from kakebo import app
-from flask import jsonify, render_template, request
-from kakebo.forms import MovimientosForm
-import sqlite3
+from flask import jsonify, render_template, request, redirect, url_for, flash
+from kakebo.forms import MovimientosForm, FiltraMovimientosForm
+from datetime import date
+from kakebo.dataaccess import *
+
+dbManager = DBmanager()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    conexion = sqlite3.connect("movimientos.db")
-    cur = conexion.cursor()
-    cur.execute("SELECT * FROM movimientos;")
-    claves = cur.description
-    filas = cur.fetchall()
-    movimientos = []
+    filtraForm = FiltraMovimientosForm()
+    query = "SELECT * FROM movimientos WHERE 1 = 1"
+    parametros = []
+
+    if request.method == 'POST':
+        if filtraForm.validate():
+            if filtraForm.fechaDesde.data != None:
+                query += " AND fecha >= ?" 
+                parametros.append(filtraForm.fechaDesde.data)
+            if filtraForm.fechaHasta.data != None:
+                query += " AND fecha <= ?"
+                parametros.append(filtraForm.fechaHasta.data)
+            if filtraForm.texto.data != '':
+                query += ' AND concepto LIKE ?'
+                parametros.append("%{}%".format(filtraForm.texto.data))  
+
+    query += " ORDER BY fecha"
+    print(query)
+    movimientos = dbManager.consultaMuchasSQL(query, parametros)
+
     saldo = 0
-    for fila in filas:
-        d = {}
-        for tclave, valor in zip(claves, fila):
-            d[tclave[0]] = valor
-            print(d)
+    for d in movimientos:
         if d['esGasto'] == 0:
             saldo = saldo + d['cantidad']
         else:
             saldo = saldo - d['cantidad']
         d['saldo'] = saldo
-        movimientos.append(d)
-    conexion.close()
-    return render_template('movimientos.html', datos = movimientos)
+    return render_template('movimientos.html', datos = movimientos, formulario = filtraForm)
+    
 
 @app.route('/nuevo', methods=['GET', 'POST'])
 def nuevo():
-    form = MovimientosForm()
-    return render_template('alta.html', form = form) #instanciamos el form del alta.html
+    formulario = MovimientosForm()
+
+    if request.method == 'GET':
+        return render_template('alta.html', form = formulario)
+    else:
+
+        if formulario.validate():
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (?, ?, ?, ?, ?)"
+            try:
+                dbManager.modificaTablaSQL(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+                                formulario.esGasto.data, formulario.cantidad.data])
+
+            except sqlite3.Error as el_error:
+                print("Error en SQL INSERT", el_error)
+                flash("Se ha producido un error en la base de datos. Pruebe en unos minutos", "error")
+                return render_template('alta.html', form=formulario)
+
+            return redirect(url_for("index"))
+
+            #Redirect a la ruta /
+        else:
+            return render_template('alta.html', form = formulario)
+
+
+@app.route('/borrar/<int:id>', methods=['GET', 'POST'])
+def borrar(id):
+    if request.method == 'GET':
+        filas = dbManager.consultaUnaSQL("SELECT * FROM movimientos WHERE id=?", [id])
+        if len(filas) == 0:
+            flash("El registro no existe", "error")
+            return render_template('borrar.html', )
+        
+        return render_template('borrar.html', movimiento=filas[0])
+    else:
+        try:
+            dbManager.modificaTablaSQL("DELETE FROM movimientos WHERE id = ?;", [id])
+        except sqlite3.error as e:
+            flash("Se ha producido un error de base de datos, vuelva a intentarlo", 'error')
+            return redirect(url_for('index'))
+            
+        flash("Borrado realizado con éxito", 'aviso')    
+        return redirect(url_for('index'))
+        
+@app.route('/modificar/<int:id>', methods=['GET', 'POST'])
+def modificar(id):
+    if request.method == 'GET':
+        registro = dbManager.consultaUnaSQL("SELECT * FROM movimientos WHERE id=?", [id])
+        if not registro == 0:
+            flash("El registro no existe", "error")
+            return render_template('modificar.html', )
+        registro['fecha'] = date.fromisoformat(registro['fecha'])
+
+        formulario = MovimientosForm(data=registro)
+        
+        return render_template('modificar.html', form=formulario)
+
+    if request.method == 'POST':
+        formulario = MovimientosForm()
+        if formulario.validate():
+            try:
+                dbManager.modificaTablaSQL("UPDATE movimientos SET fecha = ?, concepto = ?, categoria = ?, esGasto = ?, cantidad = ? WHERE id = ?",
+                                [formulario.fecha.data,
+                                formulario.concepto.data,
+                                formulario.categoria.data,
+                                formulario.esGasto.data,
+                                formulario.cantidad.data,
+                                id]
+                )
+                flash("Modificación realizada con éxito", "aviso")
+                return redirect(url_for("index"))
+            except sqlite3.Error as e:
+                print("Error en update:", e)
+                flash("Se ha producido un error en acceso a base de datos. Contacte con administrador", "error")
+                return render_template('modificar.html', form=formulario)
+        else:
+            return render_template('modificar.html', form=formulario)
